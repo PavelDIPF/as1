a3b6bf7ac831bc94e9dbce62ae152a54372d932e
PavelDIPF <pavlodipf@gmail.com>
2024-12-23 18:05:59 +0200

    Update scr.lua

--- scr.lua
+++ scr.lua
@@ -949,9 +949,9 @@ local function Hide(notify: boolean?)
 	Debounce = true
 	if notify then
 		if useMobilePrompt then 
-			RayfieldLibrary:Notify({Title = "Интерфейс Скрыт", Content = "Интерфейс был скрыт, вы можете отобразить интерфейс, нажав "Показать CasualScript".", Duration = 7, Image = 4400697855})
+			RayfieldLibrary:Notify({Title = "Interface Hidden", Content = "The interface has been hidden, you can unhide the interface by tapping 'Show Rayfield'.", Duration = 7, Image = 4400697855})
 		else
-			RayfieldLibrary:Notify({Title = "Интерфейс Скрыт", Content = "Интерфейс был скрыт, вы можете показать интерфейс, нажав K.", Duration = 7, Image = 4400697855})
+			RayfieldLibrary:Notify({Title = "Interface Hidden", Content = "The interface has been hidden, you can unhide the interface by tapping K.", Duration = 7, Image = 4400697855})
 		end
 	end
 
